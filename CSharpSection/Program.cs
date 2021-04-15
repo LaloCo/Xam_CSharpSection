b7ba129c70ec7981192a2bad6318ba008c775347
Eduardo Rosas <lalorosas@outlook.com>
2021-04-15 14:15:37 -0500

    Methods

--- CSharpSection/Program.cs
+++ CSharpSection/Program.cs
@@ -4,10 +4,18 @@ namespace CSharpSection
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine(Add(432, 543));
+
             Console.ReadLine();
+        }
+
+        private static int Add(int n1, int n2)
+        {
+            return n1 + n2;
         }
     }
 }
